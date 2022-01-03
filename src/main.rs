929fa81f7f30a3f0b6e6b646343affdd017ea9cb
Martin Seon <hyunmui@outlook.kr>
2022-01-04 08:25:23 +0900

    overlap function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-fn largest(numbers: &[i32]) -> i32 {
-    let mut largest = numbers[0];
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    for &number in numbers {
-        if number > largest {
-            largest = number;
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
@@ -12,10 +24,14 @@ fn largest(numbers: &[i32]) -> i32 {
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("Largest Number: {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
+    println!("Largest Number: {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
     println!("Largest Number: {}", result);
 }
