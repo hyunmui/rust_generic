a414bacf72d42b492a3b546c6825b9dfd0d36700
Martin Seon <hyunmui@outlook.kr>
2022-01-04 08:23:27 +0900

    refactor with function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
+fn largest(numbers: &[i32]) -> i32 {
+    let mut largest = numbers[0];
+
+    for &number in numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("Largest Number: {}", largest);
+    let result = largest(&number_list);
+    println!("Largest Number: {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("Largest Number: {}", largest);
+    let result = largest(&number_list);
+    println!("Largest Number: {}", result);
 }
