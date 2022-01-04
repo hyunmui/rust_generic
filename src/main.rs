319aac4b90991a3a80ee5f8fa35efecc9b536be8
Martin Seon <hyunmui@outlook.kr>
2022-01-05 08:46:28 +0900

    fix largest

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-// generic with function (draft, not working)
-// fn largest(list: &[T]) -> T {
-//     let mut largest = list[0];
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-//     for &item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
 
-//     largest
-// }
+    largest
+}
 
 struct Point<T> {
     x: T,
@@ -50,17 +49,17 @@ enum Option<T> {
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("Largest Number: {}", result);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("Largest Number: {}", result);
 
-    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    // let result = largest(&number_list);
-    // println!("Largest Number: {}", result);
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("Largest Number: {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest(&char_list);
-    // println!("Largest Number: {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("Largest Number: {}", result);
 
     let both_integer = Point2 { x: 5, y: 10 };
     let both_float = Point2 { x: 1.0, y: 4.0 };
