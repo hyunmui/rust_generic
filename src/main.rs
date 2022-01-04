0c3b1e8e9a27ed4895dbb09787ed30aa13da73bb
Martin Seon <hyunmui@outlook.kr>
2022-01-04 10:00:11 +0900

    practice generic

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,75 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+// generic with function (draft, not working)
+// fn largest(list: &[T]) -> T {
+//     let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
+//     largest
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
+// 구조체에 제네릭 메서드 주입
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
 
-    for &item in list {
-        if item > largest {
-            largest = item;
+// 특정 구체화된 타입의 제네릭 선언
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
         }
     }
+}
 
-    largest
+enum Option<T> {
+    Some(T),
+    None,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
-    println!("Largest Number: {}", result);
+    // let number_list = vec![34, 50, 25, 100, 65];
+    // let result = largest(&number_list);
+    // println!("Largest Number: {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&number_list);
-    println!("Largest Number: {}", result);
+    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // let result = largest(&number_list);
+    // println!("Largest Number: {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
-    println!("Largest Number: {}", result);
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    // let result = largest(&char_list);
+    // println!("Largest Number: {}", result);
+
+    let both_integer = Point2 { x: 5, y: 10 };
+    let both_float = Point2 { x: 1.0, y: 4.0 };
+    let integer_and_float = Point2 { x: 5, y: 4.0 };
+
+
+    let p1 = Point2 {x: 5, y: 10.4};
+    let p2 = Point2{ x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
